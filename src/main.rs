553602bd547bd589c5fa68aefae3f7419abab448
TgZ39 <badbaboimbus+github@gmail.com>
2024-02-18 11:44:03 +0100

    use clap as argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 use conways_game_of_life::cgof::Board;
-use std::{env, thread, time};
+use std::{thread, time};
+use clap::Parser;
+
+/// Conway's game of life [rust implementation]
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+
+    /// Length of the emulation board
+    #[arg(short, long, default_value_t = 20)]
+    board_length: u32,
+
+    /// Delay before new iteration of board is displayed. (in ms)
+    #[arg(short, long, default_value_t = 100)]
+    delay_millis: u64,
+}
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    let mut board = Board::new(args[1].parse().unwrap_or(30));
+    let mut board = Board::new(args.board_length);
 
     loop {
         println!("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         board.print();
         board.step();
-        thread::sleep(time::Duration::from_millis(args[2].parse().unwrap_or(75)));
+        thread::sleep(time::Duration::from_millis(args.delay_millis));
     }
 }
