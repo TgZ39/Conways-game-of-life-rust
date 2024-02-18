9ec9854f9b930c1419119efa8ea5a2a83015f346
TgZ39 <badbaboimbus+github@gmail.com>
2024-02-18 11:34:49 +0100

    rename project

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use cgof2::cgof::Board;
-use std::{thread, time};
+use conways_game_of_life::cgof::Board;
+use std::{env, thread, time};
 
 fn main() {
-    let mut board = Board::new(30);
+
+    let args: Vec<String> = env::args().collect();
+
+    let mut board = Board::new(args[1].parse().unwrap_or(30));
 
     loop {
         println!("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         board.print();
         board.step();
-        thread::sleep(time::Duration::from_millis(75));
+        thread::sleep(time::Duration::from_millis(args[2].parse().unwrap_or(75)));
     }
 }
